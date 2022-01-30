2a18002fb4e49419ec5cf753ecf34d58cc4180f7
Hector Carrion <hcarrion@ucsc.edu>
2022-01-30 12:47:52 -0400

    depth 7

--- Player.py
+++ Player.py
@@ -297,7 +297,7 @@ class AIPlayer:
 
         alpha = -100000
         beta  =  100000
-        depth = 4
+        depth = 7
         best_val = 0
         # random col thats avail
         best_col = random.choice(state.possible_moves())[1]
