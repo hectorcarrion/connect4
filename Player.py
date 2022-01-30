2e3ad264bc827ac344981fc04f90deb3778db743
Hector Carrion <hcarrion@ucsc.edu>
2022-01-30 14:52:32 -0400

    depth 5

--- Player.py
+++ Player.py
@@ -311,7 +311,7 @@ class AIPlayer:
 
         alpha = float('-inf')
         beta  = float('inf')
-        depth = 6
+        depth = 5
         best_val = 0
         # random col thats avail
         best_col = random.choice(state.possible_moves())[1]
